fd0da686b5fa9121b94809e2991d437846252605
Aot25922 <aottanapat25922@gmail.com>
2022-04-11 11:14:43 +0700

    connect to local backend and show detail on home page

---
+++ store/index.js
@@ -0,0 +1,10 @@
+export const state = () => ({
+    DBUrl:'http://localhost:3002',
+    // DBUrl:'',
+  });
+  
+  export const mutations = {
+    increment(state) {
+      state.counter++;
+    }
+  };
