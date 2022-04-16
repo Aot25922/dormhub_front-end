b12ab30643161fbed2d5ed1d4716b4f6667e1e84
Aot25922 <aottanapat25922@gmail.com>
2022-04-16 12:00:15 +0700

    edit URL

--- pages/dorm/index.vue
+++ pages/dorm/index.vue
@@ -1,5 +1,5 @@
 <template>
 	<div>
-		This is index
+		
 	</div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
 	<div class="p-8">
+		<nuxt-link :to="{ name: 'dorm', params: { userId: 123 }}" >test</nuxt-link>
 		<DormInfo v-for="dorm in ip" :Dorm='dorm' :key='dorm.dormId' class="mt-4 mx-5"/>
 	</div>
 </template>
