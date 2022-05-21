8d518f276282d892b3f37c76ff9a5a4256c1daf8
nithach2321 <Nithach@outlook.co.th>
2022-05-21 14:24:28 +0700

    Test CI-CD hardcode Backend_URL

--- store/index.js
+++ store/index.js
@@ -1,5 +1,5 @@
 export const state = () => ({
-    Backend_URL: process.env.Backend_URL || 'https://dev-node.dormhub.works',
+    Backend_URL: 'https://dev-node.dormhub.works',
     dorm :null
   });
 
