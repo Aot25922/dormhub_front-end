636167ab6342991c79a275893dd18a86df754baa
“Aot25922” <thanapat.work@outlook.com>
2022-08-27 12:05:05 +0700

    room form fixbug

--- components/Register/DormRoomForm.vue
+++ components/Register/DormRoomForm.vue
@@ -1,129 +1,124 @@
 <template>
   <div>
-    <div v-for="floor in floors" :key="floor.roomCount">
-      <h1>ชั้น {{ floor.floor }}</h1>
-      <div
-        v-if="$store.state.newDorm.roomType.length > 0"
-        class="grid grid-cols-4 gap-4"
-      >
-        <div v-for="(i, index) in room[floor]" :key="index" class="relative">
-          <div class="w-full bg-cream rounded-md p-3">
-            <span v-show="false">{{ (i.floors = floor) }}</span>
-            <button
-              @click="$delete(room[floor], index)"
-              class="absolute top-0 right-0"
-              v-if="room[floor].length > 1"
-            >
-              <span class="material-icons text-black">close</span>
-            </button>
-            <label
-              for=""
-              class="label-text text-gray-soil tracking-wide font-bold my-2"
-              >เลขห้อง</label
-            >
-            <input
-              type="text"
-              class="
-                py-4
-                mb-3
-                px-2
-                w-full
-                input-sm
-                rounded
-                text-gray-soil
-                bg-cream-light
-                border-0
-                focus:outline-none focus:bg-cream-lightest focus:text-gray-soil
-                disabled:bg-green-darker disabled:text-dark-gray
-              "
-              placeholder="101"
-              v-model="i.roomNum"
-            />
-            <label
-              for=""
-              class="label-text text-gray-soil tracking-wide font-bold my-2"
-              >สถานะ</label
-            >
-            <input
-              type="text"
-              class="
-                py-4
-                mb-3
-                px-2
-                w-full
-                input-sm
-                rounded
-                text-gray-soil
-                bg-cream-light
-                border-0
-                focus:outline-none focus:bg-cream-lightest focus:text-gray-soil
-                disabled:bg-green-darker disabled:text-dark-gray
-              "
-              placeholder="ว่าง/ไม่ว่าง"
-              v-model="i.status"
-            />
-            <!-- <p class="text-error text-right mt-2" v-if="!validateStatus">กรูณาใส่สถานะห้อง</p> -->
-            <!-- <p class="text-error text-right mt-2" v-if="!validateFloors">ระบุชั้นของห้องพัก</p> -->
-
-            <label
-              for=""
-              class="label-text text-gray-soil tracking-wide font-bold my-2"
-              >รายละเอียดเพิ่มเติม</label
-            >
-            <input
-              type="text"
-              class="
-                py-4
-                mb-3
-                px-2
-                w-full
-                input-sm
-                rounded
-                text-gray-soil
-                bg-cream-light
-                border-0
-                focus:outline-none focus:bg-cream-lightest focus:text-gray-soil
-                disabled:bg-green-darker disabled:text-dark-gray
-              "
-              placeholder="หอมชื่นใจ"
-              v-model="i.description"
-            />
-
-            <label
-              for=""
-              class="label-text text-gray-soil tracking-wide font-bold my-2"
-              >ประเภทห้อง</label
-            >
-            <select
-              class="
-                select
-                mb-5
-                w-full
-                text-gray-soil
-                bg-dark-gray
-                border-2 border-gray-soil
-                disabled:bg-gratext-gray-soil disabled:text-dark-blue
-              "
-              v-model="i.roomType"
-            >
-              <option option disabled selected>กรุณาเลือกประเภทห้อง</option>
-              <option
-                v-for="option in $store.state.newDorm.roomType"
-                :value="option.type"
-                :key="option.type"
-              >
-                {{ option.type }}
-              </option>
-            </select>
-          </div>
-        </div>
-        <div class="py-3">
-          <button class="text-black">
-            <span class="material-icons" @click="addNewRoom(floor.floor)"
-              >add_box</span
-            >เพิ่มห้อง
+    <div
+      v-for="(floor, index) in roomList"
+      :key="index"
+      class="grid grid-cols-4 gap-4"
+    >
+      <span>ชั้น {{ floor.floor }}</span>
+      <div v-for="(room, index) in floor.rooms" :key="index">
+        <div class="w-full bg-cream rounded-md p-3">
+          <button
+            @click="$delete(room, index)"
+            class="absolute top-0 right-0"
+            v-if="floor.rooms.length > 1"
+          >
+            <span class="material-icons text-black">close</span>
           </button>
+          <label
+            for=""
+            class="label-text text-gray-soil tracking-wide font-bold my-2"
+            >เลขห้อง</label
+          >
+          <input
+            type="text"
+            class="
+              py-4
+              mb-3
+              px-2
+              w-full
+              input-sm
+              rounded
+              text-gray-soil
+              bg-cream-light
+              border-0
+              focus:outline-none focus:bg-cream-lightest focus:text-gray-soil
+              disabled:bg-green-darker disabled:text-dark-gray
+            "
+            placeholder="101"
+            v-model="room.roomNum"
+          />
+          <label
+            for=""
+            class="label-text text-gray-soil tracking-wide font-bold my-2"
+            >สถานะ</label
+          >
+          <input
+            type="text"
+            class="
+              py-4
+              mb-3
+              px-2
+              w-full
+              input-sm
+              rounded
+              text-gray-soil
+              bg-cream-light
+              border-0
+              focus:outline-none focus:bg-cream-lightest focus:text-gray-soil
+              disabled:bg-green-darker disabled:text-dark-gray
+            "
+            placeholder="ว่าง/ไม่ว่าง"
+            v-model="room.status"
+          />
+
+          <label
+            for=""
+            class="label-text text-gray-soil tracking-wide font-bold my-2"
+            >รายละเอียดเพิ่มเติม</label
+          >
+          <input
+            type="text"
+            class="
+              py-4
+              mb-3
+              px-2
+              w-full
+              input-sm
+              rounded
+              text-gray-soil
+              bg-cream-light
+              border-0
+              focus:outline-none focus:bg-cream-lightest focus:text-gray-soil
+              disabled:bg-green-darker disabled:text-dark-gray
+            "
+            placeholder="หอมชื่นใจ"
+            v-model="room.description"
+          />
+
+          <label
+            for=""
+            class="label-text text-gray-soil tracking-wide font-bold my-2"
+            >ประเภทห้อง</label
+          >
+          <select
+            class="
+              select
+              mb-5
+              w-full
+              text-gray-soil
+              bg-dark-gray
+              border-2 border-gray-soil
+              disabled:bg-gratext-gray-soil disabled:text-dark-blue
+            "
+            v-model="room.roomType"
+          >
+            <option option disabled selected>กรุณาเลือกประเภทห้อง</option>
+            <option
+              v-for="option in $store.state.newDorm.roomType"
+              :value="option.type"
+              :key="option.type"
+            >
+              {{ option.type }}
+            </option>
+          </select>
         </div>
+      </div>
+      <div class="py-3">
+        <button class="text-black">
+          <span class="material-icons" @click="addNewRoom(floor)">add_box</span
+          >เพิ่มห้อง
+        </button>
       </div>
     </div>
     <button class="btn btn-primary" @click="addNewFloor">
@@ -135,29 +130,35 @@
 export default {
   data() {
     return {
-      room: {
-        1: [
-          {
-            roomNum: "",
-            status: "",
-            floors: 0,
-            description: "",
-            roomType: "",
-          },
-        ],
-      },
-      floors: [{floor:1, roomCount:0}],
+      roomList: [
+        {
+          floor: 1,
+          rooms: [
+            {
+              roomNum: "",
+              status: "",
+              floors: 1,
+              description: "",
+              roomType: "",
+            },
+          ],
+        },
+      ],
     };
   },
   methods: {
     submit() {
-      for (let i in this.room) {
-        this.room[i] = this.room[i].filter(
-          (x) => !(x.roomNum == "" || x.status == "" || x.roomType == "")
+      let newRoomList = []
+      for (let i in this.roomList) {
+        this.roomList[i].rooms = this.roomList[i].rooms.filter(
+          x => !(x.roomNum == "" || x.status == "" || x.roomType == "")
         );
+        for(let j in this.roomList[i].rooms){
+          newRoomList.push(this.roomList[i].rooms[j])
+        }
       }
-      let newRoom = JSON.parse(JSON.stringify(this.room));
-      this.$store.commit("SET_ROOM", newRoom);
+      newRoomList = JSON.parse(JSON.stringify(newRoomList));
+      this.$store.commit("SET_ROOM", newRoomList);
       this.$emit("validate", true);
     },
     removeRoomType(index, roomType) {
@@ -165,34 +166,29 @@ export default {
       this.$delete(this.roomTypeCount, index);
     },
     addNewFloor() {
-      this.floors.push({floor:this.floors[this.floors.length-1].floor + 1, roomCount:0});
-      this.room[this.floors[this.floors.length-1].floor + 1] = [
-        {
-          roomNum: "",
-          status: "",
-          floors: 0,
-          description: "",
-          roomType: "",
-        },
-      ];
-      console.log(this.room);
+      let newFloor = this.roomList[this.roomList.length -1].floor +1
+      this.roomList.push({
+        floor: newFloor,
+        rooms: [
+          {
+            roomNum: "",
+            status: "",
+            floors: newFloor,
+            description: "",
+            roomType: "",
+          },
+        ],
+      });
     },
     addNewRoom(floor) {
-      console.log(this.floors[floor-1])
-      this.floors[floor-1].roomCount++
-      this.room[floor].push({
+      floor.rooms.push({
         roomNum: "",
         status: "",
-        floors: 0,
+        floors: floor.floor,
         description: "",
         roomType: "",
       });
     },
   },
-  // computed: {
-  //   roomlist(){
-  //     return this.room
-  //   },
-  // }
 };
 </script>
