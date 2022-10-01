c25cfc6f7c5087f1a50be0a2415751633854762b
“Aot25922” <thanapat.work@outlook.com>
2022-10-01 14:04:51 +0700

    Create booking view

--- pages/booking/index.vue
+++ pages/booking/index.vue
@@ -1,6 +1,50 @@
 <template>
-  <div class="bg-ghostWhite px-5 py-20 md:py-32 md:grid md:grid-cols-2">
-    {{ bookingList }}
+  <div class="center">
+    <vs-table>
+      <template #thead>
+        <vs-tr>
+          <vs-th> ชื่อหอพัก </vs-th>
+          <vs-th> ประเภทห้องพัก </vs-th>
+          <vs-th> ราคา </vs-th>
+          <vs-th> เลขห้องพัก </vs-th>
+          <vs-th> วันที่เริ่มเข้าพัก </vs-th>
+          <vs-th> วันที่สิ้นสุดการเข้าพัก </vs-th>
+          <vs-th> สถานะ </vs-th>
+        </vs-tr>
+      </template>
+      <template #tbody v-if="bookingList">
+        <vs-tr
+          :key="i"
+          v-for="(tr, i) in $vs.getPage(bookingList, page, max)"
+          :data="tr"
+        >
+          <vs-td>
+            {{ tr.room.roomType.dorms[0].name }}
+          </vs-td>
+          <vs-td>
+            {{ tr.room.roomType.type }}
+          </vs-td>
+          <vs-td>
+            {{ tr.room.roomType.dorms[0].dormHasRoomType.price }}
+          </vs-td>
+          <vs-td>
+            {{ tr.room.roomNum }}
+          </vs-td>
+          <vs-td>
+            {{ tr.startDate }}
+          </vs-td>
+          <vs-td>
+            {{ tr.endDate }}
+          </vs-td>
+          <vs-td>
+            {{ tr.status }}
+          </vs-td>
+        </vs-tr>
+      </template>
+      <template #footer>
+        <vs-pagination v-if="bookingList" v-model="page" :length="$vs.getLength(bookingList, max)" />
+      </template>
+    </vs-table>
   </div>
 </template>
 
@@ -15,12 +59,14 @@ export default {
       this.bookingList = await this.$axios.$get(
         `${this.$store.state.Backend_URL}/booking/owner/${this.$store.state.userAccount.userId}`
       );
-    } else{
-      this.$router.push({path:"/"})
+    } else {
+      this.$router.push({ path: "/" });
     }
   },
   data() {
     return {
+      page: 1,
+      max: 5,
       bookingList: null,
     };
   },
