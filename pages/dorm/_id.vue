404133fc3b8f8388421a9b9fdf172707fc35d235
“Aot25922” <thanapat.work@outlook.com>
2022-10-12 11:57:24 +0700

    fix bug dorm id page

--- pages/dorm/_id.vue
+++ pages/dorm/_id.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-gray-50">
-    <div class="bg-ghostWhite px-5 py-12 xl:py-20 xl:px-52 2xl:px-96">
+    <div class="bg-ghostWhite px-5 py-12 xl:py-20 xl:px-52 2xl:px-96" v-if="dorm">
       <div class="breadcrumbs text-gray-500 md:text-lg lg:text-xl">
         <ul>
           <li>หน้าหลัก</li>
